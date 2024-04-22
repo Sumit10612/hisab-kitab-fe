c95568a81f531a93434cc35d81fdfacf0aa24008
Sumit Kumar <42916297+Sumit10612@users.noreply.github.com>
2024-04-22 20:29:47 +0000

    feat: hosting + actions

---
+++ src/app/app.config.ts
@@ -0,0 +1,31 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMBjtKAO5RZXUggmiljSbKh_EQdzoPUBs",
+  authDomain: "hisab-kitab-d6b5d.firebaseapp.com",
+  projectId: "hisab-kitab-d6b5d",
+  storageBucket: "hisab-kitab-d6b5d.appspot.com",
+  messagingSenderId: "300743409801",
+  appId: "1:300743409801:web:c65735e9941625fb7b8658"
+};
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth())
+    ]), 
+    MatSnackBarModule
+  ]
+};
