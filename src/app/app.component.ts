fd114715b010370c18a83377b4b2f6539f177ead
Sumit Kumar <suku01@betsson.com>
2024-05-04 00:31:09 +0530

    feat: pwa improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,6 +8,7 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { NotificationService } from './services/notification.service';
 import { CommonModule } from '@angular/common';
 import { UserService } from './services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -60,13 +61,27 @@ import { UserService } from './services/user.service';
       left: 50%;
       transform: translate(-50%, -50%);
     }
-
-    a {
-        cursor: pointer;
-      }
   `]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly snackBar = inject(MatSnackBar);
+
   protected readonly userService = inject(UserService);
   protected readonly notificationService = inject(NotificationService);
+
+  ngOnInit() {
+    if(window.matchMedia("display-mode: browser").matches) {
+      if (!("standalone" in navigator)) {
+        window.addEventListener("beforeinstallprompt", event => {
+          event.preventDefault();
+          const sb = this.snackBar.open("You can install this app", "Install", { duration: 5000 });
+          sb.onAction().subscribe(() => {
+            (event as any).prompt();
+          });
+        });
+      } else {
+        this.snackBar.open("You can install this app, use Share > Add to home screeen", "", { duration: 5000 });
+      }
+    }
+  }
 }
