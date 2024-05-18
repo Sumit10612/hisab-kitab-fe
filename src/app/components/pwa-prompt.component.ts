4f075a0aacaa89c7ecab9827cfda3fdf33526bdd
Sumit Kumar <suku01@betsson.com>
2024-05-19 03:10:42 +0530

    fix: build error

--- src/app/components/pwa-prompt.component.ts
+++ src/app/components/pwa-prompt.component.ts
@@ -48,7 +48,7 @@ import { MatListModule } from "@angular/material/list";
 })
 export class PwaPromptComponent {
 	constructor(
-    @Inject(MAT_BOTTOM_SHEET_DATA) public data: { platform: "ios" | "android"; event?: unknown },
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: { platform: "ios" | "android"; event?: any },
     private bottomSheetRef: MatBottomSheetRef<PwaPromptComponent>
 	) {}
 
