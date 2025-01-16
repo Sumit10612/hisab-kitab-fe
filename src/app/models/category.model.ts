68ca532092e93ac21f1da0ebc549cc94b4597493
Sumit Kumar <suku01@betsson.com>
2025-01-16 13:30:18 +0530

    more categories

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -34,6 +34,7 @@ export const categoriesByGroup: CategoryGroup[] = [
 			{ id: 104, name: "Investment", icon: "🏦" },
 			{ id: 105, name: "Bills", icon: "🧾" },
 			{ id: 106, name: "EMI", icon: "💸" },
+			{ id: 107, name: "Stationary", icon: "🖊️" },
 		]
 	},
 	{
@@ -44,7 +45,11 @@ export const categoriesByGroup: CategoryGroup[] = [
 			{ id: 602, name: "Train", icon: "🚂" },
 			{ id: 603, name: "Flight", icon: "🛩️" },
 			{ id: 604, name: "Cab", icon: "🚕" },
-			{ id: 605, name: "Ferry", icon: "🚢" }
+			{ id: 605, name: "Ferry", icon: "🚢" },
+			{ id: 606, name: "Auto", icon: "🛺" },
+			{ id: 607, name: "Toll", icon: "🛣️" },
+			{ id: 608, name: "Parking", icon: "🅿️" },
+			{ id: 609, name: "Hotel", icon: "🏨" },
 		]
 	},
 	{
@@ -52,7 +57,7 @@ export const categoriesByGroup: CategoryGroup[] = [
 		categories: [
 			{ id: 201, name: "Shopping", icon: "🛍️" },
 			{ id: 202, name: "Movies", icon: "🎥" },
-			{ id: 203, name: "Entertainment", icon: "🎞️" },
+			{ id: 203, name: "Entertainment", icon: "🎭" },
 			{ id: 204, name: "Sports", icon: "🏸" },
 			{ id: 205, name: "Salon", icon: "💇‍♀️" },
 		]
@@ -61,23 +66,26 @@ export const categoriesByGroup: CategoryGroup[] = [
 		name: "Food & drink",
 		categories: [
 			{ id: 301, name: "Dining", icon: "🍽️" },
-			{ id: 302, name: "Groceries", icon: "🧺" },
+			{ id: 302, name: "Groceries", icon: "🛒" },
 			{ id: 303, name: "Liquor", icon: "🍺" },
-			{ id: 304, name: "Vegitable", icon: "🫛" },
+			{ id: 304, name: "Vegitable", icon: "🥦" },
 			{ id: 305, name: "Dairy", icon: "🥛" },
 			{ id: 306, name: "Food", icon: "🍔" },
 			{ id: 307, name: "Beverage", icon: "🍵" },
 			{ id: 308, name: "Non Veg", icon: "🍗" },
-			{ id: 309, name: "Fruits", icon: "🍇 " }
+			{ id: 309, name: "Fruits", icon: "🍎" },
+			{ id: 310, name: "Snacks", icon: "🍿" },
 		]
 	},
 	{
-		name: "Home",
+		name: "Home & Maintenance",
 		categories: [
-			{ id: 401, name: "Electronics", icon: "📺" },
+			{ id: 401, name: "Electronics", icon: "📱" },
 			{ id: 402, name: "Furniture", icon: "🪑" },
 			{ id: 403, name: "Household supplies", icon: "🏠" },
 			{ id: 404, name: "Maintenance", icon: "🛠️" },
+			{ id: 405, name: "Car", icon: "🚗" },
+			{ id: 406, name: "Bike", icon: "🏍️" },
 		]
 	},
 	{
@@ -85,7 +93,7 @@ export const categoriesByGroup: CategoryGroup[] = [
 		categories: [
 			{ id: 501, name: "Bank", icon: "🏦" },
 			{ id: 502, name: "Stocks", icon: "📈" },
-			{ id: 503, name: "MFs", icon: "📊"},
+			{ id: 503, name: "MFs", icon: "📊" },
 			{ id: 504, name: "Gold", icon: "🪙"},
 			{ id: 505, name: "Gov Bonds", icon: "🪪"}
 		]
